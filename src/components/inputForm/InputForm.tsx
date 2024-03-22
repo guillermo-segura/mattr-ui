01d1b71a7f24200f176874f65eb6d2db827c53c1
Guillermo Segura <correo@seguraguillermo.net>
2024-03-22 10:43:12 +0000

    chore: added input form classnames file

--- src/components/inputForm/InputForm.tsx
+++ src/components/inputForm/InputForm.tsx
@@ -1,6 +1,7 @@
 import React, { InputHTMLAttributes } from 'react';
 
 import Input from '../input/Input';
+import css from './classnames'
 
 export interface InputFormProps {
   label: string;
@@ -12,7 +13,7 @@ export interface InputFormProps {
 const InputForm = ({ label, value, onChange, inputProps }: InputFormProps): JSX.Element => {
   const onInputChange = (e: React.FormEvent<HTMLInputElement>) => onChange(e.currentTarget.value);
   return (
-    <div className="flex flex-col space-y-1">
+    <div className={css.container}>
       <label>{label}</label>
       <Input value={value} onChange={onInputChange} {...inputProps} />
     </div>
---
+++ src/components/inputForm/classnames.ts
@@ -0,0 +1,5 @@
+const classnames = {
+  container: 'flex flex-col space-y-1',
+};
+
+export default classnames;
