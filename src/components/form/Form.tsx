97e1554cc635e8377886dedec3ebf7cea2e1c981
Guillermo Segura <correo@seguraguillermo.net>
2024-03-22 14:03:36 +0000

    refactor: form cleanup

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -21,16 +21,44 @@ const Form = () => {
     submitValues,
   } = useForm();
 
-
   return (
     <div className={css.container} data-testid="form-container">
-      <InputForm label="Given name" value={givenName} onChange={setGivenName} />
-      <InputForm label="Email" value={email} onChange={setEmail} inputProps={{ type: 'email' }} />
-      <InputForm label="Country of residence" value={countryOfResidence} onChange={setCountryOfResidence} />
-      <InputForm label="Date of birth" value={dateOfBirth} onChange={setDateOfBirth} inputProps={{ type: 'date' }} />
-      <InputForm label="Photo" value={photo} onChange={setPhoto} inputProps={{ type: 'file' }} />
+      <InputForm
+        label="Given name"
+        value={givenName}
+        onChange={setGivenName}
+        inputProps={{ type: 'email', placeholder: 'E.g. John Doe' }}
+      />
+      <InputForm
+        label="Email"
+        value={email}
+        onChange={setEmail}
+        inputProps={{ type: 'email', placeholder: 'john@email.com' }}
+      />
+      <InputForm
+        label="Country of residence"
+        value={countryOfResidence}
+        onChange={setCountryOfResidence}
+        inputProps={{ placeholder: 'New Zealand' }}
+      />
+      <InputForm
+        label="Date of birth"
+        value={dateOfBirth}
+        onChange={setDateOfBirth}
+        inputProps={{ type: 'date' }}
+      />
+      <InputForm
+        label="Photo"
+        value={photo}
+        onChange={setPhoto}
+        inputProps={{ type: 'file' }}
+      />
       <br />
-      <Button label={isSubmitting ? 'Submitting' : 'Submit'} onClick={submitValues} disabled={isSubmitting} />
+      <Button
+        label={isSubmitting ? 'Submitting' : 'Submit'}
+        onClick={submitValues}
+        disabled={isSubmitting}
+      />
     </div>
   );
 }
