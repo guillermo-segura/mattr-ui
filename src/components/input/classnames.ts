26c831f1faffab41cb26cb0542f904b8a7e6c2bf
Guillermo Segura <correo@seguraguillermo.net>
2024-03-22 14:08:18 +0000

    feat: updated placeholder text color

--- src/components/input/classnames.ts
+++ src/components/input/classnames.ts
@@ -1,6 +1,6 @@
 const classnames = {
-  input: 'cursor-pointer rounded-sm h-12 text-black px-2 outline-0 w-full bg-white border border-black focus:border-mattr-blue',
-  fileInput: 'file:bg-mattr-blue-light file:border-mattr-blue-dark file:text-mattr-blue-dark file:px-4 file:py-1 file:w-fit file:rounded-full file:self-center file:border-0 file:border file:uppercase file:font-semibold file:cursor-pointer pt-1.5 file:drop-shadow-md hover:file:bg-mattr-blue-dark hover:file:text-white',
+  input: 'cursor-pointer rounded-sm h-12 text-black px-2 outline-0 w-full bg-white border border-black focus:border-mattr-blue placeholder:text-[#635E61]',
+  fileInput: 'file:bg-mattr-blue-light file:border-mattr-blue-dark file:text-mattr-blue-dark file:px-4 file:py-1 file:w-fit file:rounded-full file:self-center file:border file:border-solid file:uppercase file:font-semibold file:cursor-pointer pt-1.5 file:drop-shadow-md hover:file:bg-mattr-blue-dark hover:file:text-white',
 };
 
 export default classnames;
